02cf701e8496c5da07328db94891413595e3cd5b
slivcaigor <slivcaigor@gmail.com>
2023-01-25 15:00:18 +0100

    Cucce + Display Product Details

--- cucce.php
+++ cucce.php
@@ -10,5 +10,34 @@ class Cucce extends Prodotto
         $this->dimensione = $dimensione;
         $this->materiale = $materiale;
     }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    public function getPrezzo(): string
+    {
+        return $this->prezzo;
+    }
+
+    public function getDimensione(): string
+    {
+        return $this->dimensione;
+    }
+    public function getMateriale(): string
+    {
+        return $this->materiale;
+    }
 }
+$prodotto1 = new Cucce("Cuccia in legno", "Cuccia in legno per cani di piccola taglia", 50, "60x40x50cm", "legno");
+$prodotto2 = new Cucce("Cuccia in plastica", "Cuccia in plastica per gatti", 30, "50x35x40cm", "plastica");
+$prodotto3 = new Cucce("Cuccia in tessuto", "Cuccia in tessuto per animali domestici", 40, "50x35x40cm", "tessuto");
+
+$prodotti = [$prodotto1, $prodotto2, $prodotto3];
 ?>
--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+    </script>
     <title>Pets Store</title>
 
     <?php
@@ -18,8 +24,28 @@
 
 <body>
 
-    <h1>Welcome to Pets Store!</h1>
-
+    <div class="card">
+        <div class="card-body">
+            <?php foreach ($prodotti as $prodotto) { ?>
+            <!-- <img src="https://picsum.photos/200/300" class="card-img-top"> -->
+            <h5 class="card-title">
+                <?= $prodotto->getNome() ?>
+            </h5>
+            <p class="card-text">
+                <?= $prodotto->getDescrizione() ?>
+            </p>
+            <p class="card-text">
+                <?= $prodotto->getDimensione() ?>
+            </p>
+            <p class="card-text">
+                <?= $prodotto->getMateriale() ?>
+            </p>
+            <p class="card-text">
+                <?= $prodotto->getPrezzo() . ' $' ?>
+            </p>
+            <?php } ?>
+        </div>
+    </div>
 </body>
 
 </html>
